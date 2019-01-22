62003fa70a659163b08b289c208fafc58a739baa
Ole Bak Jensen <obj@trifork.com>
2019-01-22 15:36:20 +0100

    Fix typescript build problem

--- fsk-online-ts/src/main/ts/webpack.d.ts
+++ fsk-online-ts/src/main/ts/webpack.d.ts
@@ -1,8 +1,10 @@
-declare var require: {
-  <T>(path: string): T;
-  (paths: string[], callback: (...modules: any[]) => void): void;
-  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
+interface WebpackRequire {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+}
+interface NodeRequire extends WebpackRequire {}
+declare var require: NodeRequire;
 
 declare var SCM_VERSION: string;
 declare var SCM_BRANCH: string;
