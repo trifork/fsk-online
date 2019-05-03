e51870cd572e29b1d9da09c260f775e2a0bee6d2
Ole Bak Jensen <obj@trifork.com>
2019-05-03 10:17:25 +0200

    SDS-3451 More text added to treatment will warning popup

--- fsk-online-ts/src/main/ts/app/tabs/DoctorOrNurseWillTab.ts
+++ fsk-online-ts/src/main/ts/app/tabs/DoctorOrNurseWillTab.ts
@@ -127,13 +127,15 @@ export default class DoctorOrNurseWillTab extends TemplateWidget implements Tabb
         const yesClicked = await PopupDialog.display(
             PopupDialogKind.WARNING,
             "Bekræft",
-            "<h4 style='font-size:18px'>Visning af testamente</h4><br>" +
-            "Testamente bør kun fremsøges såfremt<br><br>" +
-            "<ul style='list-style: inherit; padding-left:32px'>" +
-            "<li>Patienten ligger for døden (dvs. er uafvendeligt døende)</li>" +
-            "<li>Patienten er hjælpeløs pga. sygdom, ulykke mv., og der ikke er tegn på bedring</li>" +
-            "</ul>" +
-            "<br>Bemærk: Tilgang til data for testamentet vil blive logget, givet at der er data. Tilgang vil fremgå af patientens minlog.</li>",
+            `<h4 style='font-size:18px'>Visning af testamente</h4><br>
+            Testamente bør kun fremsøges såfremt<br><br>
+            <ul style='list-style: inherit; padding-left:32px'>
+            <li>Patienten ligger for døden (dvs. er uafvendeligt døende)</li>
+            <li>Patienten er hjælpeløs pga. sygdom, ulykke mv., og der ikke er tegn på bedring</li>
+            <li>Behandlingen kan føre til overlevelse, men de fysiske konsekvenser af sygdommen eller behandlingen vurderes at være meget alvorlige og lidelsesfulde</li>
+            <li>Man overvejer at iværksætte behandling ved anvendelse af tvang efter lov om anvendelse af tvang ved somatisk behandling af varigt inhabile</li>
+            </ul>
+            <br>Bemærk: Tilgang til data for testamentet vil blive logget, givet at der er data. Tilgang vil fremgå af patientens minlog.`,
             noOption,
             yesOption);
         if (yesClicked === yesOption) {
