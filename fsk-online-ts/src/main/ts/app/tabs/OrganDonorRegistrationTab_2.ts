5ddd71a32f551be02fa24a7058f46b31a56c5ace
shbo <shbo@trifork.com>
2025-02-12 15:05:19 +0100

    FMKO-1785 HTML færdigopsat, mangler stadig funktionalitet

--- fsk-online-ts/src/main/ts/app/tabs/OrganDonorRegistrationTab_2.ts
+++ fsk-online-ts/src/main/ts/app/tabs/OrganDonorRegistrationTab_2.ts
@@ -3,7 +3,14 @@ import {Component, Dependency, Injector, Render, WidgetElement} from "fmko-ts-mv
 import {IoC} from "fmko-ts-ioc";
 import FSKService from "../services/FSKService";
 import FSKOrganDonorCache from "../services/FSKOrganDonorCache";
-import {ImageDimensions, InfoPanel, InfoPanelSeverity, WCAGRadioButton} from "fmko-ts-widgets";
+import {
+    ButtonStyle,
+    ImageDimensions,
+    InfoPanel,
+    InfoPanelSeverity,
+    StyledButton,
+    WCAGRadioButton
+} from "fmko-ts-widgets";
 import FullAccessPermissionPanel from "../panels/organdonor-panels/FullAccessPermissionPanel";
 import LimitedAccessPermissionPanel from "../panels/organdonor-panels/LimitedAccessPermissionPanel";
 
@@ -20,8 +27,8 @@ export default class OrganDonorRegistrationTab_2 implements TabbedPanel, Render 
     private initialized: boolean;
     private shown: boolean;
 
-    private fullAccessPanel: FullAccessPermissionPanel;
-    private limitedAccessPanel: LimitedAccessPermissionPanel;
+    @WidgetElement private fullPermissionPanel: FullAccessPermissionPanel;
+    @WidgetElement private limitedPermissionPanel: LimitedAccessPermissionPanel;
 
     @WidgetElement private mainPanel: HTMLDivElement;
     @WidgetElement private registrationDate: InfoPanel;
@@ -33,6 +40,13 @@ export default class OrganDonorRegistrationTab_2 implements TabbedPanel, Render 
     @WidgetElement private limitedPermissionWithResearchRadio: WCAGRadioButton<FSKTypes.OrganDonorPermissionType>;
     @WidgetElement private dontKnowPermissionRadio: WCAGRadioButton<FSKTypes.OrganDonorPermissionType>;
     @WidgetElement private restrictedPermissionRadio: WCAGRadioButton<FSKTypes.OrganDonorPermissionType>;
+
+    @WidgetElement private createButton: StyledButton;
+    @WidgetElement private updateButton: StyledButton;
+    @WidgetElement private deleteButton: StyledButton;
+    @WidgetElement private printButton: StyledButton;
+
+    @WidgetElement private noRegistrationText: HTMLSpanElement;
 
     constructor(
         @Injector private container: IoC,
@@ -52,8 +66,9 @@ export default class OrganDonorRegistrationTab_2 implements TabbedPanel, Render 
     }
 
     public render(): void | Promise<never> {
+        const registrationDate = "en eller anden dato"; // TODO fix
         this.registrationDate = new InfoPanel({
-            description: "Registreringen er bekræftet og ændret: DATO",
+            description: `Registreringen er bekræftet og ændret: ${registrationDate}`,
             severity: InfoPanelSeverity.INFO,
             imageOptions: {
                 alt: "info sign",
@@ -86,17 +101,25 @@ export default class OrganDonorRegistrationTab_2 implements TabbedPanel, Render 
             checkedValue: "DONT_KNOW",
             label: ""
         });
-        this.dontKnowPermissionRadio.element.querySelector("label").innerHTML =
-            "<span>Patienten <strong>overlader det i stedet til sine nærmeste pårørende</strong> " +
-            "at tage stilling til, om patientens organer må anvendes til transplantation og forskning " +
-            "efter sin død</span>";
+        const dontKnowStrongTextElement = document.createElement("strong");
+        dontKnowStrongTextElement.textContent = "overlader det i stedet til sine nærmeste pårørende";
+        this.dontKnowPermissionRadio.element.querySelector("label").append(
+            "Patienten ",
+            dontKnowStrongTextElement,
+            " at tage stilling til, om patientens organer må anvendes til transplantation og forskning ",
+            "efter sin død"
+        );
         this.restrictedPermissionRadio = new WCAGRadioButton({
             checkedValue: "NONE",
             label: ""
         });
-        this.restrictedPermissionRadio.element.querySelector("label").innerHTML =
-            "<span>Patienten <strong>modsætter sig, at patientens organer anvendes</strong> " +
-            "til transplantation efter sin død</span>";
+        const restrictedStrongTextElement = document.createElement("strong");
+        restrictedStrongTextElement.textContent = "modsætter sig, at patientens organer anvendes";
+        this.restrictedPermissionRadio.element.querySelector("label").append(
+            "Patienten ",
+            restrictedStrongTextElement,
+            " til transplantation efter sin død"
+        );
 
         this.radioGroup.push(
             this.fullPermissionWithResearchRadio,
@@ -111,6 +134,45 @@ export default class OrganDonorRegistrationTab_2 implements TabbedPanel, Render 
             radioButton.addValueChangeHandler(() =>
                 this.setValue(radioButton.getCheckedValue()));
         });
+
+        this.fullPermissionPanel = this.container.resolve(FullAccessPermissionPanel);
+        this.limitedPermissionPanel = this.container.resolve(LimitedAccessPermissionPanel);
+
+        this.createButton = new StyledButton({
+            text: "Opret registrering",
+            style: ButtonStyle.DEFAULT,
+            clickHandler: () => {
+                // TODO Do something
+            }
+        });
+        this.updateButton = new StyledButton({
+            text: "Opdater registrering",
+            style: ButtonStyle.DEFAULT,
+            clickHandler: () => {
+                // TODO Do something
+            }
+        });
+        this.deleteButton = new StyledButton({
+            text: "Slet registrering",
+            style: ButtonStyle.DEFAULT,
+            clickHandler: () => {
+                // TODO Do something
+            }
+        });
+        this.printButton = new StyledButton({
+            text: "Print",
+            style: ButtonStyle.DEFAULT,
+            clickHandler: () => {
+                // TODO Do something
+            }
+        });
+
+        const patientNameElement = document.createElement("strong");
+        patientNameElement.textContent = "Dude"; // TODO fix navn
+        this.noRegistrationText.append(
+            patientNameElement,
+            " har ikke bekræftet sin tilmelding til Organdonorregistreret"
+        );
     }
 
     public getId(): string {
@@ -195,10 +257,10 @@ export default class OrganDonorRegistrationTab_2 implements TabbedPanel, Render 
             if (type === "FULL" || type === "FULL_WITH_RESEARCH") {
                 return {
                     permissionType: type,
-                    requiresRelativeAcceptance: this.fullAccessPanel.getRequiresRelativeAcceptance()
+                    requiresRelativeAcceptance: this.fullPermissionPanel.getRequiresRelativeAcceptance()
                 };
             } else if (type === "LIMITED" || type === "LIMITED_WITH_RESEARCH") {
-                const value = this.limitedAccessPanel.getValue();
+                const value = this.limitedPermissionPanel.getValue();
                 if (value) {
                     value.permissionType = type;
                 }
